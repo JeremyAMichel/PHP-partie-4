095b79f6960a2aa545f7607d63cf10f082ba5780
JeremyAMichel <jeremy.a.michel@hotmail.fr>
2023-06-27 21:01:28 +0200

    correction

---
+++ index.php
@@ -0,0 +1,64 @@
+<?php 
+
+// exercice 1 //
+// function returnTrue(){
+//     return true;
+// }
+
+// exercice 2 //
+// function returnString($string){
+//     return $string;
+// }
+
+// exercice 3 //
+// function returnConcat($string1, $string2){
+//     return $string1.$string2;
+// }
+
+// exercice 4 //
+// function compareNumber($number1, $number2){
+//     if($number1 > $number2){
+//         return 'Le premier nombre est plus grand';
+//     } else if($number1 < $number2){
+//         return 'Le premier nombre est plus petit';
+//     } else {
+//         return 'Les deux nombres sont identiques';
+//     }
+// }
+
+// exercice 5 //
+// function concatNumberstring($number, $string){
+//     return $number.' '.$string;
+// }
+
+// exercice 6 //
+// function bonjourVousAvez($nom, $prenom, $age){
+//     return 'Bonjour '.$nom.' '.$prenom.', tu as '.$age.' ans';
+// }
+
+// exercice 7 //
+// function genderAge($gender, $age){
+//     if($gender === 'homme'){
+//         $result='Vous êtes un homme ';
+//         if($age >= 18){
+//             $result = $result . 'et vous êtes majeur';
+//         } else {
+//             $result = $result . 'et vous êtes mineur';
+//         }
+//     } else {
+//         $result='Vous êtes une femme ';
+//         if($age >= 18){
+//             $result = $result . 'et vous êtes majeur';
+//         } else {
+//             $result = $result . 'et vous êtes mineur';
+//         }
+//     }
+// }
+
+// exercice 8 //
+// function sumNumber($number1=0, $number2=0, $number3=0){
+//     return $number1 + $number2 + $number3;
+// }
+
+
+?>
